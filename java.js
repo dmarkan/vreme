4656968af564be4f9700d8f08b99dd9423996ef3
Darko Markan <75615049+dmarkan@users.noreply.github.com>
2021-01-18 19:20:34 +0100

    Initial commit

---
+++ java.js
@@ -0,0 +1,180 @@
+function forma() {
+
+var bg = document.querySelector("#grad input").value;
+var bg2 = document.querySelector("#grad input").value;
+var ns = document.querySelector("#grad input").value;
+var ns2 = document.querySelector("#grad input").value;
+var ni = document.querySelector("#grad input").value;
+var ni2 = document.querySelector("#grad input").value;
+var krg = document.querySelector("#grad input").value;
+var krg2 = document.querySelector("#grad input").value;
+var pr = document.querySelector("#grad input").value;
+var pr2 = document.querySelector("#grad input").value;
+var sub = document.querySelector("#grad input").value;
+var sub2 = document.querySelector("#grad input").value;
+var zr = document.querySelector("#grad input").value;
+var zr2 = document.querySelector("#grad input").value;
+var pa = document.querySelector("#grad input").value;
+var pa2 = document.querySelector("#grad input").value;
+var ca = document.querySelector("#grad input").value;
+var ca2 = document.querySelector("#grad input").value;
+var krus = document.querySelector("#grad input").value;
+var krus2 = document.querySelector("#grad input").value;
+var kralj = document.querySelector("#grad input").value;
+var kralj2 = document.querySelector("#grad input").value;
+var np = document.querySelector("#grad input").value;
+var np2 = document.querySelector("#grad input").value;
+var smed = document.querySelector("#grad input").value;
+var smed2 = document.querySelector("#grad input").value;
+var lesk = document.querySelector("#grad input").value;
+var lesk2 = document.querySelector("#grad input").value;
+var uz = document.querySelector("#grad input").value;
+var uz2 = document.querySelector("#grad input").value;
+var vr = document.querySelector("#grad input").value;
+var vr2 = document.querySelector("#grad input").value;
+var so = document.querySelector("#grad input").value;
+var so2 = document.querySelector("#grad input").value;
+var poz = document.querySelector("#grad input").value;
+var poz2 = document.querySelector("#grad input").value;
+var pi = document.querySelector("#grad input").value;
+var pi2 = document.querySelector("#grad input").value;
+var zaj = document.querySelector("#grad input").value;
+var zaj2 = document.querySelector("#grad input").value;
+var ki = document.querySelector("#grad input").value;
+var ki2 = document.querySelector("#grad input").value;
+var sm = document.querySelector("#grad input").value;
+var sm2 = document.querySelector("#grad input").value;
+var jg = document.querySelector("#grad input").value;
+var jg2 = document.querySelector("#grad input").value;
+var vrs = document.querySelector("#grad input").value;
+var vrs2 = document.querySelector("#grad input").value;
+var bor = document.querySelector("#grad input").value;
+var bor2 = document.querySelector("#grad input").value;
+var pro = document.querySelector("#grad input").value;
+var pro2 = document.querySelector("#grad input").value;
+var lo = document.querySelector("#grad input").value;
+var lo2 = document.querySelector("#grad input").value;
+var valj = document.querySelector("#grad input").value;
+var valj2 = document.querySelector("#grad input").value;
+var sa = document.querySelector("#grad input").value;
+var sa2 = document.querySelector("#grad input").value;
+if(bg === "Beograd") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-beograd-srbija-sr/7-dana"; 
+} else if (bg2 === "beograd") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-beograd-srbija-sr/7-dana"; 
+} else if (ns === "Novi Sad") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-novi-sad-serbia-sr/7-dana";
+} else if (ns2 === "novi sad") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-novi-sad-serbia-sr/7-dana";
+} else if (ni === "Nis") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-ni%C5%A1-srbija-sr/7-dana";
+} else if (ni2 === "nis") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-ni%C5%A1-srbija-sr/7-dana"; 
+} else if (krg === "Kragujevac") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-kragujevac-serbia-sr/7-dana"; 
+} else if (krg2 === "kragujevac") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-kragujevac-serbia-sr/7-dana"; 
+} else if (pr === "Pristina") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-pri%C5%A1tina-srbija-sr/7-dana"; 
+} else if (pr2 === "pristina") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-pri%C5%A1tina-srbija-sr/7-dana"; 
+} else if (sub === "Subotica") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-subotica-serbia-sr/7-dana";
+} else if (sub2 === "subotica") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-subotica-serbia-sr/7-dana";
+} else if (zr === "Zrenjanin") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-zrenjanin-serbia-sr/7-dana";
+} else if (zr2 === "zrenjanin") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-zrenjanin-serbia-sr/7-dana";
+} else if (pa === "Pancevo") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-pan%C4%8Devo-srbija-sr/7-dana";
+} else if (pa2 === "pancevo") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-pan%C4%8Devo-srbija-sr/7-dana";
+} else if (ca === "Cacak") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-%C4%8Da%C4%8Dak-srbija-sr/7-dana";
+} else if (ca2 === "cacak") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-%C4%8Da%C4%8Dak-srbija-sr/7-dana";
+} else if (krus === "Krusevac") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-kru%C5%A1evac-srbija-sr/7-dana";
+} else if (krus2 === "krusevac") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-kru%C5%A1evac-srbija-sr/7-dana";
+} else if (kralj === "Kraljevo") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-kraljevo-serbia-sr/7-dana";
+} else if (kralj2 === "kraljevo") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-kraljevo-serbia-sr/7-dana";
+} else if (np === "Novi Pazar") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-novi-pazar-serbia-sr/7-dana";
+} else if (np2 === "novi pazar") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-novi-pazar-serbia-sr/7-dana";
+} else if (smed === "Smederevo") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-smederevo-serbia-sr/7-dana";
+} else if (smed2 === "smederevo") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-smederevo-serbia-sr/7-dana";
+} else if (lesk === "Leskovac") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-leskovac-serbia-sr/7-dana";
+} else if (lesk2 === "leskovac") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-leskovac-serbia-sr/7-dana";
+} else if (uz === "Uzice") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-u%C5%BEice-srbija-sr/7-dana";
+} else if (uz2 === "uzice") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-u%C5%BEice-srbija-sr/7-dana";
+} else if (vr === "Vranje") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-vranje-serbia-sr/7-dana";
+} else if (vr2 === "vranje") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-vranje-serbia-sr/7-dana";
+} else if (so === "Sombor") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-sombor-serbia-sr/7-dana";
+} else if (so2 === "sombor") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-sombor-serbia-sr/7-dana";
+} else if (poz === "Pozarevac") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-pozarevac-serbia-sr/7-dana";
+} else if (poz2 === "pozarevac") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-pozarevac-serbia-sr/7-dana";
+} else if (pi === "Pirot") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-pirot-serbia-sr/7-dana";
+} else if (pi2 === "pirot") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-pirot-serbia-sr/7-dana";
+} else if (zaj === "Zajecar") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-zaje%C4%8Dar-srbija-sr/7-dana";
+} else if (zaj2 === "zajecar") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-zaje%C4%8Dar-srbija-sr/7-dana";
+} else if (ki === "Kikinda") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-kikinda-serbia-sr/7-dana";
+} else if (ki2 === "kikinda") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-kikinda-serbia-sr/7-dana";
+} else if (sm === "Sremska Mitrovica") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-sremska-mitrovica-serbia-sr/7-dana";
+} else if (sm2 === "sremska mitrovica") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-sremska-mitrovica-serbia-sr/7-dana";
+} else if (jg === "Jagodina") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-jagodina-serbia-sr/7-dana";
+} else if (jg2 === "jagodina") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-jagodina-serbia-sr/7-dana";
+} else if (vrs === "Vrsac") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-vr%C5%A1ac-srbija-sr/7-dana";
+} else if (vrs2 === "vrsac") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-vr%C5%A1ac-srbija-sr/7-dana";
+} else if (bor === "Bor") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-bor-serbia-sr/7-dana";
+} else if (bor2 === "bor") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-bor-serbia-sr/7-dana";
+} else if (pro === "Prokuplje") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-prokuplje-serbia-sr/7-dana";
+} else if (pro2 === "prokuplje") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-prokuplje-serbia-sr/7-dana";
+} else if (lo === "Loznica") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-loznica-serbia-sr/7-dana";
+} else if (lo2 === "loznica") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-loznica-serbia-sr/7-dana";
+} else if (valj === "Valjevo") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-valjevo-serbia-sr/7-dana";
+} else if (valj2 === "valjevo") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-valjevo-serbia-sr/7-dana";
+} else if (sa === "Sabac") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-sabac-serbia-sr/7-dana";
+} else if (sa2 === "sabac") {
+window.location.href = "https://www.weather2umbrella.com/vremenska-prognoza-sabac-serbia-sr/7-dana";
+} else {
+alert("Nisu pronadjeni podaci o vremenu za zadati grad, odaberi neki drugi grad");
+}
+}
